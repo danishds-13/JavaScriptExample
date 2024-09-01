edef8f8a77019a03fc9ec8ca481f691645fe82dc
danish_ds <danishdeepasoman02@gmail.com>
2024-09-01 22:55:37 +0530

    primitives and objects
    Learning of primitives and objects

--- README.md
+++ README.md
@@ -6,4 +6,8 @@ This Repo is made to learn around the use of javascript
         Var - Can be used to define a variable that is in global view to the script so if i use anywhere its gonna take that value 
         let - It is a block statement so if in a scope i use the block statement then i can change it. 
         const - Cannot be changed its always stagnent this helps hence we can use const when we want to make a variable constant and not changeable (this would throw an error).
-3. 
+3. Primitive data types in javascript - there are 7 types of datatypes in JS that is - Null, Number, Symbol, String, Boolean, Bigint, Undefined 
+   Objects are a key value string - in python its called as dictionary but in the js its called as objects 
+
+
+
--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-// Initial command to display anything we use the console.log
+// Initial command to display anything we use the console.log - POINT1
 console.log("helloworld");
 
-//to print something using an variable 
+//to print something using an variable -POINT1
 let a1=64
 console.log(a)
 
-//To show var let and const 
+//To show var let and const -Point-2
 var a = 45;
 let b ="harry";
 var c = null
@@ -17,3 +17,23 @@ const e = "hellowarzone"
 }
 console.log(b)
 console.log(e)
+
+// To show about primitive data types oin javascript - Point-3
+let a2 = null;
+let b2 = 75;
+let c2 = "dan"
+let d2 = Symbol("I am learning things for a better tomorrow")
+let e2 = true;
+let f2 = BigInt("567") + BigInt("3")
+let g2 = undefined
+console.log(a2 ,b2 ,c2 ,d2 ,e2 ,f2 ,g2)
+console.log(typeof d2)
+
+//Objects - POINT-3  - In python this is called dictionary then in JS its called as objects 
+const team ={
+    "harry" : true,
+    "kiran": false,
+    "lokesh": 67,
+    "rohan": undefined
+}
+console.log(team["harry"])
